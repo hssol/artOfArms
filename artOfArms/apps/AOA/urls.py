9b3c077a03681426144972c669267cedf88def2f
k <cyxrus0@gmail.com>
2019-07-28 22:05:13 -0700

    added blog page

--- artOfArms/apps/AOA/urls.py
+++ artOfArms/apps/AOA/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^art_of_arms$', views.main_page)
+    url(r'^art_of_arms$', views.main_page),
+    url(r'^art_of_arms/blogs$', views.blog_page)
 ]
--- artOfArms/apps/AOA/views.py
+++ artOfArms/apps/AOA/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'AOA/main_page.html')
-# Create your views here.
+
+def blog_page(request):
+    return render(request, 'AOA/blog_page.html')
